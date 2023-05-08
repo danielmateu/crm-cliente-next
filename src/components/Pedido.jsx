66d8a49d1203433786e29bf8d47ced0fe5aa19b3
Daniel Mateu <danielmateu86@gmail.com>
2023-05-08 12:10:50 +0200

    Mostrar info de los pedidos

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -9,10 +9,9 @@ mutation eliminarPedido($id :ID!){
 `
 
 export const Pedido = ({ pedido }) => {
-    
-    const { id, total, cliente: { nombre, apellido}, estado } = pedido;
-    
-    
+
+    const { id, total, cliente: { nombre, apellido, telefono, email }, estado } = pedido;
+    // console.log(pedido);
     const [estadoPedido, setEstadoPedido] = useState(estado)
 
     useEffect(() => {
@@ -60,10 +59,21 @@ export const Pedido = ({ pedido }) => {
 
     return (
         <main className='flex justify-between bg-gray-200 p-4 rounded mb-4 shadow-lg'>
-            <div className='flex flex-col gap-2 mb-4 '>
-                <p className='font-bold'>Cliente: {nombre} {apellido}</p>
+            <div className='flex flex-col gap-1 mb-4 '>
+                <p className='font-semibold flex'>Cliente: {nombre} {apellido}</p>
+                {
+                    email && <p className='font-semibold flex items-center'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+                        <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
+                    </svg><span className='font-semibold text-gray-500'> : {email}</span></p>
+                }
+                {
+                    telefono && <p className='font-semibold flex items-center'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                    </svg>
+                        <span className='font-semibold text-gray-500'> : {telefono}</span></p>
+                }
                 {/* <p>Estado pedido: </p> */}
-                <select name="" id="" className='p-2 appearance-none rounded leading-tight outline-none'
+                <select name="" id="" className='p-2 appearance-none rounded leading-tight outline-none mt-2'
                     value={estadoPedido}
                     onChange={handleChange}
                 >
@@ -98,7 +108,6 @@ export const Pedido = ({ pedido }) => {
                     onClick={eliminarPedido}
                 >
                     Eliminar Pedido
-
                 </button>
             </div>
         </main>
